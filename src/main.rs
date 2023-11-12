a82eb7cd2ada4290aac3a0fb904c106517da6cce
Sam Jones <land.sj@proton.me>
2023-11-12 16:38:51 -0500

    changed mouse api+position support

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ use windows::Win32::UI::WindowsAndMessaging::{ShowWindow, SW_HIDE, SW_SHOW};
 use std::process::exit;
 
 use eframe::{egui, emath::Align};
-use rdev::{simulate, EventType, Button};
 use colored::Colorize;
 use global_hotkey::{GlobalHotKeyManager, GlobalHotKeyEvent, hotkey::{HotKey, Code}};
+use inputbot::{MouseButton::LeftButton, MouseCursor};
 
 mod border;
 
@@ -19,12 +19,22 @@ fn print_error<S: std::fmt::Display>(e: S) {
 	eprintln!("Line {}: {}", std::panic::Location::caller().line(), format!("{}", e).red());
 }
 
+#[derive(PartialEq)]
+enum RadioEnum {
+	Follow,
+	Position,
+}
+
 struct App {
 	is_clicking: bool,
 	last_click: std::time::Instant,
 	delay_ms_str: String,
 	delay_ms: u128,
 	console_visible: bool,
+	follow_mouse: RadioEnum,
+	position: Vec<i32>,
+	waiting_for_click: bool,
+	debug_mode: bool,
 }
 
 impl Default for App {
@@ -35,27 +45,34 @@ impl Default for App {
 			delay_ms_str: "0".to_string(),
 			delay_ms: 0,
 			console_visible: false,
+			follow_mouse: RadioEnum::Follow,
+			position: vec![0, 0],
+			waiting_for_click: false,
+			debug_mode: false,
 		}
 	}
 }
 
 impl App {
 	fn click(&self) {
-		simulate(&EventType::ButtonPress(Button::Left)).unwrap_or_else(|e| {
-			print_error(e);
-			exit(1);
-		});
-		simulate(&EventType::ButtonRelease(Button::Left)).unwrap_or_else(|e| {
-			print_error(e);
-			exit(1);
-		});
+		// simulate(&EventType::ButtonPress(Button::Left)).unwrap_or_else(|e| {
+		// 	print_error(e);
+		// 	exit(1);
+		// });
+		// simulate(&EventType::ButtonRelease(Button::Left)).unwrap_or_else(|e| {
+		// 	print_error(e);
+		// 	exit(1);
+		// });
+		LeftButton.press();
+		LeftButton.release();
 	}
 
-	fn click_pos<T: Into<f64> + Copy>(&self, pos: Vec<T>) {
-		simulate(&EventType::MouseMove { x: pos[0].into(), y: pos[1].into() }).unwrap_or_else(|e| {
-			print_error(e);
-			exit(1);
-		});
+	fn click_pos<T: Into<i32> + Copy>(&self, pos: Vec<T>) {
+		// simulate(&EventType::MouseMove { x: pos[0].into(), y: pos[1].into() }).unwrap_or_else(|e| {
+		// 	print_error(e);
+		// 	exit(1);
+		// });
+		MouseCursor::move_abs(pos[0].into(), pos[1].into());
 		self.click();
 	}
 }
@@ -90,19 +107,18 @@ impl eframe::App for App {
 
 			border::custom_window_frame(ctx, frame, "Auto Clicker", |ui| {
 				ui.vertical(|ui| {
-					let seperation = 24.0;
 					let rect = ui.available_rect_before_wrap();
 					let mut left = rect.clone();
-					left.set_right(222.5);
-					left.set_bottom(rect.top()+10.0);
+					left.set_right(148.3333);
+					left.set_bottom(rect.top()+20.0);
 					let mut right = rect.clone();
-					right.set_left(222.5);
+					right.set_left(148.3333);
 					right.set_right(437.0);
 					right.set_bottom(rect.top()+10.0);
 
 					ui.allocate_ui_at_rect(left, |ui| {
 						ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
-							ui.add_space(seperation*0.5);
+							// ui.add_space(seperation*0.5);
 							ui.label("ms");
 							if ui.add(egui::TextEdit::singleline(&mut self.delay_ms_str).hint_text("0")).changed() {
 								self.delay_ms_str = self.delay_ms_str.chars().filter(|x| x.to_string().parse::<u128>().is_ok()).collect();
@@ -117,21 +133,32 @@ impl eframe::App for App {
 							}
 						});
 					});
-
+					
 					ui.allocate_ui_at_rect(right, |ui| {
-						ui.with_layout(egui::Layout::left_to_right(Align::Center), |ui| {
-							ui.add_space(seperation*0.5);
+						ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
+							// ui.add_space(seperation*0.5);
+							if ui.add_enabled(self.follow_mouse == RadioEnum::Position, egui::Button::new("Set position")).clicked() {
+								self.waiting_for_click = true;
+								println!("waiting for next click");
+							}
+							ui.radio_value(&mut self.follow_mouse, RadioEnum::Position, "Position: ");
+							ui.radio_value(&mut self.follow_mouse, RadioEnum::Follow, "Follow mouse");
+
+							// ui.label("X");
+							// ui.add(egui::TextEdit::singleline(&mut self.position[0]).interactive(false));
+							// ui.label("Y");
+							// ui.add(egui::TextEdit::singleline(&mut self.position[1]).interactive(false));
 						});
 					});
 				});
 
-				ui.separator();
+				// ui.separator();
 
 				ui.vertical(|ui| {
 	
 					let mut centered = egui::text::LayoutJob::default();
 					let font_size = 16.0;
-					centered.append("Start", 0.0, egui::TextFormat {
+					centered.append("Start (F6)", 0.0, egui::TextFormat {
 						font_id: egui::FontId::new(font_size, egui::FontFamily::Proportional),
 						valign: egui::Align::Center,
 						..Default::default()
@@ -146,7 +173,7 @@ impl eframe::App for App {
 					right.set_left(222.5);
 					right.set_right(437.0);
 					right.set_bottom(rect.top()+50.0);
-	
+
 					ui.allocate_ui_at_rect(left, |ui| {
 						ui.with_layout(egui::Layout::right_to_left(Align::Center), |ui| {
 							ui.add_space(seperation*0.5);
@@ -158,7 +185,7 @@ impl eframe::App for App {
 					});
 	
 					centered.sections.clear();
-					centered.append("Stop", 0.0, egui::TextFormat {
+					centered.append("Stop (F6)", 0.0, egui::TextFormat {
 						font_id: egui::FontId::new(font_size, egui::FontFamily::Proportional),
 						valign: egui::Align::Center,
 						..Default::default()
@@ -174,11 +201,14 @@ impl eframe::App for App {
 					});
 	
 				});
-	
-				if ui.button("Toggle console (debug)").clicked() {
-					toggle_console(!self.console_visible);
-					self.console_visible = !self.console_visible;
+
+				if self.debug_mode {
+					if ui.button("Toggle console (debug)").clicked() {
+						toggle_console(!self.console_visible);
+						self.console_visible = !self.console_visible;
+					}
 				}
+
 			});
 
 		});
@@ -190,9 +220,31 @@ impl eframe::App for App {
 		}
 
 		if self.is_clicking && self.last_click.elapsed().as_millis() >= self.delay_ms {
-			self.click();
+			if self.follow_mouse == RadioEnum::Follow {
+				self.click();
+			} else {
+				self.click_pos(self.position.clone());
+			}
 			self.last_click = std::time::Instant::now();
 		}
+
+		if self.waiting_for_click {
+			egui::Window::new("Info").collapsible(false).resizable(false).show(ctx, |ui| {
+				ui.label("On the next click your cursor position will be saved!");
+			});
+
+			if LeftButton.is_pressed() {
+				self.waiting_for_click = false;
+				let (x, y) = MouseCursor::pos();
+				self.position = vec![x, y];
+				println!("position set as {}, {}", x, y);
+			}
+		}
+
+		if ctx.input(|i| i.key_pressed(egui::Key::F10)) {
+			self.debug_mode = true;
+		}
+
 		ctx.request_repaint();
 	}
 
@@ -257,6 +309,7 @@ fn main() {
 		resizable: false,
 		decorated: false,
 		transparent: true,
+		always_on_top: true,
 		min_window_size: Some(egui::vec2(445.0, 360.0)),
 		max_window_size: Some(egui::vec2(445.0, 360.0)),
 		..Default::default()
@@ -272,4 +325,5 @@ fn main() {
 	).unwrap_or_else(|e| {
 		print_error(e);
 	});
+
 }
